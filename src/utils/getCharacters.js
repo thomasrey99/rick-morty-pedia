7d01a4c459ccc550ab666924e2f4e030395018fd
Thomas leonel rey <103622621+thomasrey99@users.noreply.github.com>
2024-01-20 12:53:51 -0300

    Update getCharacters.js

--- src/utils/getCharacters.js
+++ src/utils/getCharacters.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 export const getCharacters=async(page, name, gender, status, species)=>{
 
-    const data=(await axios(`http://localhost:3000/api/characters?page=${page}&name=${name}&gender=${gender}&status=${status}&species=${species}`)).data
+    const data=(await axios(`https://rick-morty-pedia-six.vercel.app/api/characters?page=${page}&name=${name}&gender=${gender}&status=${status}&species=${species}`)).data
 
     return data
 
